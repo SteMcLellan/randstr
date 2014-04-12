523746a0fa12df965290d04361321e33350ce25a
Steve McLellan <mclellan.stephen@gmail.com>
2014-04-11 23:04:44 -0400

    Ran gofmt on codebase

--- randstr.go
+++ randstr.go
@@ -1,64 +1,64 @@
 package randstr
 
-import(
-    "math/rand"
+import (
+	"math/rand"
 )
 
 type StringSource interface {
-  Rune() rune
+	Rune() rune
 }
 
 type stringSource struct {
-  random *rand.Rand
+	random *rand.Rand
 }
 
 func (ss stringSource) Rune() rune {
-  chr := ss.random.Intn(52) + 65
-  if chr > 90 {
-    chr = chr + 6
-  }
+	chr := ss.random.Intn(52) + 65
+	if chr > 90 {
+		chr = chr + 6
+	}
 
-  return rune(chr)
+	return rune(chr)
 }
 
 type RandString struct {
-  strSrc StringSource
+	strSrc StringSource
 }
 
 func NewSource(seed int64) StringSource {
-  randSource := rand.NewSource(seed)
-  rand := rand.New(randSource)
-  ss := &stringSource{rand}
+	randSource := rand.NewSource(seed)
+	rand := rand.New(randSource)
+	ss := &stringSource{rand}
 
-  return ss
+	return ss
 }
 
 func New(ss StringSource) *RandString {
-  return &RandString{ss}
+	return &RandString{ss}
 }
 
 func (rnd *RandString) Next() rune {
-  var n rune
-  n = rnd.strSrc.Rune()
+	var n rune
+	n = rnd.strSrc.Rune()
 
-  return n
+	return n
 }
 
 func (rnd *RandString) NextStr(l int) string {
-  var n []rune
-  n = make([]rune, l)
+	var n []rune
+	n = make([]rune, l)
 
-  for i := range n {
-    n[i] = rnd.Next()
-  }
+	for i := range n {
+		n[i] = rnd.Next()
+	}
 
-  return string(n)
+	return string(n)
 }
 
 func (rnd *RandString) Read(b []byte) (n int, err error) {
-  for i := range b {
-    b[i] = byte(rnd.Next())
-  }
+	for i := range b {
+		b[i] = byte(rnd.Next())
+	}
 
-  return len(b), nil
+	return len(b), nil
 }
--- randstr_test.go
+++ randstr_test.go
@@ -1,33 +1,33 @@
 package randstr
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestNext(t *testing.T) {
-  r := New(NewSource(1))
-  c := r.Next()
+	r := New(NewSource(1))
+	c := r.Next()
 
-  if c != 'x' {
-    t.Errorf("Incorect letter (%v) returned, expected x.", c, string(c))
-  }
+	if c != 'x' {
+		t.Errorf("Incorect letter (%v) returned, expected x.", c, string(c))
+	}
 }
 
 func TestReader(t *testing.T) {
-  r := New(NewSource(1))
-  var buf = make([]byte, 10)
+	r := New(NewSource(1))
+	var buf = make([]byte, 10)
 
-  n, err := r.Read(buf)
-  if err != nil {
-    t.Errorf("Error reading from random string stream.")
-  }
+	n, err := r.Read(buf)
+	if err != nil {
+		t.Errorf("Error reading from random string stream.")
+	}
 
-  if n != 10 {
-    t.Errorf("Did not read the full length of buffer (10)")
-  }
+	if n != 10 {
+		t.Errorf("Did not read the full length of buffer (10)")
+	}
 
-  str := string(buf)
-  if str != "xvLbZGBAIc" {
-    t.Errorf("Buffer reads as %s", string(buf))
-  }
+	str := string(buf)
+	if str != "xvLbZGBAIc" {
+		t.Errorf("Buffer reads as %s", string(buf))
+	}
 }
